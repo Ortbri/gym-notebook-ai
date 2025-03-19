0e7082f25a6e6df693bc69c280d591f157947ea4
ortbri <brian.ort02@icloud.com>
2025-03-19 09:21:48 -0500

    feat:supabase

--- src/utils/betterAuth/server.ts
+++
@@ -1,7 +0,0 @@
-import { expo } from '@better-auth/expo';
-import { betterAuth } from 'better-auth';
-
-export const auth = betterAuth({
-  trustedOrigins: ['faxtions://'],
-  plugins: [expo()],
-});
