0b143481f3f5bd19f8d63d6a4e004d0c6b0f9758
Brian <brian.ort02@icloud.com>
2025-04-18 09:45:27 -0500

    fix:holy crap everything broken lol

--- metro.config.js
+++ metro.config.js
@@ -1,28 +1,17 @@
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 // eslint-disable-next-line no-undef
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
-
-// Create the default Metro config
-const config = getSentryExpoConfig(projectRoot, {
+const config = getSentryExpoConfig(__dirname, {
   //   enabled: process.env.NODE_ENV === 'production',
   enableSourceContextInDevelopment: true,
   annotateReactComponents: true,
 });
 
-// Add the additional workspace packages
-config.watchFolders = [workspaceRoot];
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-];
-
 // --- burnt ---
 config.resolver.sourceExts.push('mjs');
 config.resolver.sourceExts.push('cjs');
 // --- end burnt ---
+
+config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
